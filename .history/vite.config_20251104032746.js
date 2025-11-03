1b5bae39fbdc3e63769ad99ca0d879101bd9e9d5
maicakes <Floresjamaicamae30@gmail.com>
2025-11-04 03:28:59 +0800

    Initial commit

--- .history/vite.config_20251104032746.js
+++ .history/vite.config_20251104032746.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/news_snap/', 
+  base: '/frontend_communitrade/', // 👈 add this line
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
--- .history/vite.config_20251104032752.js
+++ .history/vite.config_20251104032752.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/news_snap/', 
+  base: '/news_snap/', // 👈 add this line
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
